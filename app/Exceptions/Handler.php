7e8f39d8cea2aa47388a1e4d2f963fd70edaa7db
Thiago Mandrik <thiagomandrik@gmail.com>
2025-05-17 16:45:43 -0300

    feat: add Pokemon API endpoint with authentication handling and basic controller

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,6 +4,7 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Auth\AuthenticationException;
 
 class Handler extends ExceptionHandler
 {
@@ -27,4 +28,10 @@ class Handler extends ExceptionHandler
             //
         });
     }
+
+    public function unauthenticated($request, AuthenticationException $exception)
+    {
+        return response()->json(['error' => 'Unauthorized'], 401);
+    }
+
 }
