358c7d4650812a4f386c216ebde77f173e804ea6
Thiago Mandrik <thiagomandrik@gmail.com>
2025-05-18 11:03:08 -0300

    feat: implement Pokemon listing and detail endpoints using PokeAPI integration

--- app/Http/Controllers/PokedexController.php
+++ app/Http/Controllers/PokedexController.php
@@ -3,15 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class PokedexController extends Controller
 {
-
-    public function getPokemon()
+    public function listPokemons(Request $request)
     {
-        $pokemon = 'blastoise';
-        return response()->json([
-            'pokemon' => $pokemon
+        $limit = $request->get('limit', 20);
+        $offset = $request->get('offset', 0);
+
+        $response = Http::get("https://pokeapi.co/api/v2/pokemon", [
+            'limit' => $limit,
+            'offset' => $offset,
         ]);
+
+        return response()->json($response->json());
+    }
+
+    public function showPokemon($name)
+    {
+        $response = Http::get("https://pokeapi.co/api/v2/pokemon/{$name}");
+
+        if ($response->failed()) {
+            return response()->json(['error' => 'Pokemon not found'], 404);
+        }
+
+        return response()->json($response->json());
     }
 }
